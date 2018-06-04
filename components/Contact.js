e4d28ab98d80250e1edb2fce43c2086118044746
Krystian Bieniek <krystekk2013@gmail.com>
2018-06-04 18:23:10 +0200

    Add new repository

---
+++ components/Contact.js
@@ -0,0 +1,22 @@
+var Contact = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+      React.createElement('div', {className: 'contactItem'},
+        React.createElement('img', {
+          className: 'contactImage',
+          src: 'https://cdn4.iconfinder.com/data/icons/round-buttons/512/blue_user.png',
+          width: 100
+        }),
+        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+          this.props.item.email
+        )
+      )
+    )
+  },
+});
